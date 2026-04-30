c4907528937fbdd2488c07073dc6c0feac464fd7
thanosda <154046417+Thanosda@users.noreply.github.com>
2026-04-30 13:52:53 +0530

    New codes are added

--- Code-19.py
+++ Code-19.py
@@ -1,4 +1,18 @@
-for i in range(3):
-    print(i)
+# to Check Armstrong Number
+num=int(input("Enter the Number :"))
+leng=len(str(num))
+temp=num
+start=0
+if num<1:
+    print("Number cant be below than zero")
 else:
-    print("Loop finished")
+    while temp>0:
+     new=temp%10
+     start=(new**leng) + start
+     temp=temp//10
+
+    if start==num:
+        print("it was a amstrong number")
+    else:
+        print("It was not a Amstrong number")
+
