601f9d61089ea8a1b40b2c55d5f03ea54f506fb2
thanosda <154046417+Thanosda@users.noreply.github.com>
2026-06-08 21:22:06 +0530

    two codes has been added

--- 141 Codes/Code-89.py
+++ 141 Codes/Code-89.py
@@ -1,1 +1,18 @@
-strings = list(map(str, [1, 2, 3]))
+#Please write a program using list comprehension to print the Fibonacci Sequence incomma separated form with a given n input by console.
+def fibonacci(n):
+
+    if n == 0:
+        return 0
+
+    elif n == 1:
+        return 1
+
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+n = int(input("Enter n: "))
+
+result = [str(fibonacci(i)) for i in range(n)]
+
+print(",".join(result))
---
+++ 141 Codes/Code-90.py
@@ -0,0 +1,11 @@
+#please write program to print the user name of a given email address. Both usernames and company names are composed of letters only.
+
+Email=input("enter your email:")
+
+sto=Email.split("@")[0]
+
+print(sto)
+
+
+
+
